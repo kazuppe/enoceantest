ddab302a443141b55a66f0375542622756afc86a
OHSE Kazuhiko <ohse.kazuhiko@gmail.com>
2018-10-16 01:33:01 +0900

    各種機器のシリアルポート対応

--- enoceantest.py
+++ enoceantest.py
@@ -1,10 +1,16 @@
 import serial
-ser = serial.Serial('/dev/tty.usbserial-FT2LMD98',57600,timeout=1)
+
+DEVICE = '/dev/tty.usbserial-FT2LMD98'  # For Mac
+# DEVICE = '/dev/ttyUSB0'    # For Raspberry Pi
+# DEVICE = 'COM7'    # For Windwos
+
+ser = serial.Serial(DEVICE,57600,timeout=1)
+
 while True:
   try:
     c = ser.read()
-    if c != b'U':
-        continue
+    if c != b'U':   # Not SYNC
+        continue    # Get next packet.
     else:
         clist = [85]
         c = ser.read(15)
